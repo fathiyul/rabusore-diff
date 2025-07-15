8db8fb898f60062cad78729d57f9de0a955f22c1
Fathiyul Fahmi <fathiyul@gmail.com>
2025-07-15 14:31:12 +0700

    text

--- hooks/use-panel-state.ts
+++ hooks/use-panel-state.ts
@@ -20,7 +20,8 @@ SPEAKER_02: It's a tool for comparing transcription texts. You can use it to see
 SPEAKER_01: That sounds useful! So it's for people who work with speech-to-text?
 SPEAKER_02: Exactly. Researchers, linguists, and developers can use it to evaluate the accuracy of transcription services.
 SPEAKER_03: What are the advanced features?
-SPEAKER_02: It offers advanced metrics like Word Error Rate and Diarization Error Rate. You can also switch between word and character diffing, and even use a normalized mode that ignores case and punctuation. There's also a word map for collections of different writing for some words that you want to ignore from errors, and you can easily add items to it.`,
+SPEAKER_02: It offers advanced metrics like Word Error Rate and Diarization Error Rate. You can also switch between word and character diffing, and even use a normalized mode that ignores case and punctuation. There's also a word map for collections of different writing for some words that you want to ignore from errors, and you can easily add items to it.
+SPEAKER_03: Hmm, nice.`,
     isVisible: true,
   },
   {
@@ -31,7 +32,8 @@ SPEAKER_02: It is a tool for comparing transcription texts. You can use it to se
 SPEAKER_01: That sounds useful! So it's for people who work with speech-to-text?
 SPEAKER_02: Exactly. Researchers, linguists, and developers can use it to valuate the accuracy of transcription services.
 SPEAKER_03: What are the advanced features?
-SPEAKER_02: It offers advanced metrics like Word Error Rate and Diarization Error Rate. You can also stitch between word and character diving, and even use a normalized mode that ignores case and punctuation. There's also a word map for collections of different writing for some words that you want to ignore from errors, and you can easily add items into it.`,
+SPEAKER_02: It offers advanced metrics like Word Error Rate and Diarization Error Rate. You can also stitch between word and character diving, and even use a normalized mode that ignores case and punctuation. There's also a word map for collections of different writing for some words that you want to ignore from errors, and you can easily add items into it.
+SPEAKER_03: Hmm, nice.`,
     isVisible: true,
   },
   {
